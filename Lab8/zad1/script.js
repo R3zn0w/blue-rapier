5afb6b7e3bd728d29380a45a88db2cb6ce292c95
Michal Belzak <michal.belzak@gmail.com>
2021-12-09 20:03:35 +0100

    fix, rename

---
+++ Lab8/zad1/script.js
@@ -0,0 +1,7 @@
+"use strict";
+function submitAction() {
+  console.log(document.forms[0].elements.pole_tekstowe.value);
+  console.log(document.forms[0].elements.pole_liczbowe.value);
+}
+
+window.prompt("Tekst1", "Tekst2");
